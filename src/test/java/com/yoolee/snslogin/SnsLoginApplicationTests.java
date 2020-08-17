124c07d4d8edb49c3738ee6d993d74dc83cd94c7
Hong Yoolee <im.yoolee@gmail.com>
2020-08-17 11:35:55 +0900

    pj commit

---
+++ src/test/java/com/yoolee/snslogin/SnsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoolee.snslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
